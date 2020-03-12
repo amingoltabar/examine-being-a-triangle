4e56cd77689c3dc740724a7c61173f01a7e8bc9f
amingoltabar <61889677+amingoltabar@users.noreply.github.com>
2020-03-12 15:06:25 +0330

    Add files via upload

---
+++ p11.py
@@ -0,0 +1,8 @@
+a=float(input('enter the first number'))
+b=float(input('enter the second number'))
+c=float(input('enter the third number'))
+if a+b>c and a+c>b and b+c>a:
+    print('its a triangle')
+else:
+    print('its not a triangle')
+    
